bc5bf7f83f556b4ec7be011e7b499e2688ea0265
BASTOLU <lucas_waiteman.bastos@daimler.com>
2019-11-21 13:37:31 -0300

    adiciona novos testes

--- test_app.py
+++ test_app.py
@@ -4,42 +4,59 @@ api_url_base = 'http://localhost:5000'
 endPoint = '/api/gianini'
 
 def testInsertUsers():
-    user = "{'email':'contato@amandagianini.com', 'cnpj':'15047359000191', 'password':'amanda'}"
+    user = "{'cnpj':'1234567890', 'email':'contato@amandagianini.com', 'password':'amanda'}"
     response = requests.post(api_url_base + endPoint + '/{0}/{1}'.format('Users', 'insert'), json=user)
 
     assert(response.json()['ResponseMetadata']['HTTPStatusCode'] == 200)
 
 def testDeleteUsers():
-    user = "{'email':'contato@amandagianini.com', 'cnpj':'15047359000191'}"
+    user = "{'cnpj':'1234567890'}"
     
     response = requests.post(api_url_base + endPoint + '/{0}/{1}'.format('Users', 'delete'), json=user)
 
     assert(response.json()['ResponseMetadata']['HTTPStatusCode'] == 200)
 
 def testQueryUsers():
-    response = requests.post(api_url_base + endPoint + '/{0}/{1}?{2}&{3}'.format('Users', 'query', 'key=email', 'email=tracuns@gmail.com'))
+    response = requests.post(api_url_base + endPoint + '/{0}/{1}?{2}&{3}'.format('Clients', 'query', 'key=cnpj', 'value=38161933000133'))
 
-    assert(response.json()[0]['cnpj'] == '1234567890123')
+    assert(response.json()[0]['cnpj'] == '38161933000133')
 
-def testUpdateUsers(ticker):
-    user = "{'email':'contato@amandagianini.com', 'cnpj':'15047359000191'}"
+def testUpdateUsers():
+    user = "{'cnpj':'15047359000191', 'email':'contato@amandagianini.com'}"
 
     response = requests.post(api_url_base + endPoint + '/{0}/{1}'.format('Users', 'update'), json=user)
 
     assert(response.json()['ResponseMetadata']['HTTPStatusCode'] == 200)
 
+def testQuerySchedulingDates():
+    response = requests.post(api_url_base + endPoint + '/{0}/{1}?{2}&{3}'.format('SchedulingDates', 'query', 'key=date', 'value=27112019'))
+
+    print(response.json()[0])
+    assert(response.json()[0]['cnpj'] == '38161933000133')
+
+def testSelect():
+    data = {'date':'27112019', 'hour':'0700'}
+
+    response = requests.post(api_url_base + endPoint + '/{0}/{1}'.format('SchedulingDates', 'select'), json=data)
+    print(response.json)
+
+    assert(response.json()['date'] == '27112019')
+
 def testLogin():
-    user = {'email':'tracuns@gmail.com', 'password':'1234'}
+    user = {'cnpj':'123456789', 'password':'1234'}
     response = requests.post(api_url_base + '/api/gianini/Users/login', json=user)
 
-    assert(response.json()[0]['email'] == 'tracuns@gmail.com')
-    assert(response.json()[0]['password'] == '1234')
+    assert(response.json()['cnpj'] == '123456789')
+    assert(response.json()['email'] == 'tracuns@gmail.com')
+    assert(response.json()['password'] == '1234')
 
 if __name__ == '__main__':
 
+    testSelect()
+    testQuerySchedulingDates()
     testLogin()
-    #testInsertUsers()
-    #testQueryUsers()
-    #testDeleteUsers()
+    testInsertUsers()
+    testQueryUsers()
+    testDeleteUsers()
     
     # testUpdateUsers()
